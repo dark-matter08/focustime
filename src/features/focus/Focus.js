049a85b8473cd050f9ec31a8371c3acc1c64511d
Nde Lucien <chelucien08@gmail.com>
2022-09-14 09:29:36 +0100

    Update Focus.js

--- src/features/focus/Focus.js
+++ src/features/focus/Focus.js
@@ -2,70 +2,56 @@
 import React, {useState} from 'react';
 import {View, Text, StyleSheet} from 'react-native';
 import {TextInput} from 'react-native-paper';
-import {RoundedButton} from '../../components'
-import { fontSizes, spacing } from '../../utils/sizes';
-import {colors} from '../../utils/colors'
+import {RoundedButton} from '../../components';
+import {fontSizes, spacing} from '../../utils/sizes';
+import {colors} from '../../utils/colors';
 
 const Focus = ({addSubject}) => {
-    const [subject, setSubject] = useState(null)
-    const submitFxn = () => {
-      console.log(subject)
-      addSubject(subject)
-    };
-    return (
-        <View style={
-            styles.container
-        }>
-            <View style={
-                styles.innerContainer
-            }>
-                <Text style={
-                    styles.title
-                }>What would you like to focus on?</Text>
-                <View style={
-                    styles.inputContainer
-                }>
-                    <TextInput 
-                      style={
-                          {
-                              flex: 1,
-                              marginRight: spacing.md
-                          }
-                      }
-                      onSubmitEditing = {
-                          ({nativeEvent}) => {
-                            setSubject(nativeEvent.text)
-                          }
-                      }
-                    />
-                    <RoundedButton size={50}
-                        title='+' onPress={submitFxn} 
-                    />
-                </View>
-            </View>
+  const [subject, setSubject] = useState(null);
+  const submitFxn = () => {
+    console.log(subject);
+    addSubject(subject);
+  };
+  return (
+    <View style={styles.container}>
+      <View style={styles.innerContainer}>
+        <Text style={styles.title}>What would you like to focus on?</Text>
+        <View style={styles.inputContainer}>
+          <TextInput
+            style={{
+              flex: 1,
+              marginRight: spacing.md,
+            }}
+            onSubmitEditing={({nativeEvent}) => {
+              setSubject(nativeEvent.text);
+            }}
+          />
+          <RoundedButton size={50} title="+" onPress={submitFxn} />
         </View>
-    );
-}
+      </View>
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 0.5
-    },
-    innerContainer: {
-        flex: 1,
-        padding: spacing.md,
-        justifyContent: 'center'
-    },
-    title: {
-        color: colors.white,
-        fontWeight: 'bold',
-        fontSize: fontSizes.lg
-    },
-    inputContainer: {
-        paddingTop: spacing.md,
-        flexDirection: 'row',
-        alignItems: 'center'
-    }
+  container: {
+    flex: 0.5,
+  },
+  innerContainer: {
+    flex: 1,
+    padding: spacing.md,
+    justifyContent: 'center',
+  },
+  title: {
+    color: colors.white,
+    fontWeight: 'bold',
+    fontSize: fontSizes.lg,
+  },
+  inputContainer: {
+    paddingTop: spacing.md,
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
 });
 
 export default Focus;
